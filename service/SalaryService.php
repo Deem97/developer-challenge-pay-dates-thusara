37e60f4b26465d62e56482861deee3a2c24753f3
Deem97 <thdeema@gmail.com>
2023-02-03 23:22:45 +0530

    Add descriptive comments to clarify code functionality

--- service/SalaryService.php
+++ service/SalaryService.php
@@ -6,6 +6,10 @@ namespace service;
 
 class SalaryService
 {
+    // Returns the salary payment date
+    // If the payment day is a weekday, return it
+    // If not, return the last weekday before the end of the month
+
     function getSalaryPaymentDate(\DateTime $date, int $month, int $year): string
     {
         $date->setDate($year, $month, 1);
